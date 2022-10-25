16bcb2607281019effe5bd2f5a7299e4d9ff11ff
Hasala <hasaladissanayake@yahoo.com>
2022-10-25 14:04:03 +0530

    dashboard added'

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+
 class Dashboard extends Component {
 
     componentDidMount() {
@@ -20,52 +21,65 @@ class Dashboard extends Component {
 
     render() {
         return (
+
             <div className='dashboard-inner'>
-                
+                <div class="text-end p-4">
+                <button type="button" className="btn btn-danger btn-lg " ><Link className="text-white text-decoration-none" to="/sign-in">Logout</Link></button>
+                </div>
+
                 <div className="row row-cols-3 g-4">
                         
                 <div className="col">
-                        <div className="card h-100">
-                        {/* <img class="card-img-top" src="../logo.svg" alt="Card image cap"></img> */}
+                        <div className="card h-100  bg-secondary bg-opacity-25" >
+                        <img src="https://trendsandtribulations.files.wordpress.com/2014/02/tommy-hilfiger-men.jpeg" class="img-fluid " alt="Card image cap"></img>
                         <div className="card-body">
-                        <h5 className="card-title"><Link className="link-success text-decoration-none" to="/dashboard/men">Men</Link></h5>
-                        <p className="card-text">Welcome to Men Section.</p>
+                        <h5 className="card-title text-dark">Men</h5>
+                        <p className="card-text text-secondary">Welcome to Men Section.</p>
+                        <a href="#" class="btn btn-danger"><Link className="link-light text-decoration-none" to="/dashboard/men">Explore</Link></a>
                     </div>
                     </div>
                 </div>
                         
                 <div className="col">
-                        <div className="card h-100">
+                        <div className="card h-100 bg-secondary bg-opacity-25">
+                        <img src="https://s.wsj.net/public/resources/images/BN-XU052_ONESHE_574V_20180308123844.jpg" class="img-fluid" alt="Card image cap"></img>
                         <div className="card-body">
-                        <h5 className="card-title"><Link className="link-success text-decoration-none" to="/dashboard/women">Women</Link></h5>
-                        <p className="card-text">Welcome to Women Section.</p>
+                        <h5 className="card-title text-dark">Women</h5>
+                        <p className="card-text text-secondary">Welcome to Women Section.</p>
+                        <a href="#" class="btn btn-danger"><Link className="link-light text-decoration-none" to="/dashboard/women">Explore</Link></a>
                     </div>
                     </div>
                 </div>
                         
                 <div className="col">
-                        <div className="card h-100">     
+                        <div className="card h-100 bg-secondary bg-opacity-25"> 
+                        <img src="https://img.freepik.com/free-photo/group-happy-smiling-teen-girls-boys-pastel-stylish-young-teen-girls-posing-classic-autumn-style-teen-kids-fashion-concept-children-s-fasion-concept_155003-14009.jpg?w=2000" class="img-fluid" alt="Card image cap"></img>    
                         <div className="card-body">
-                        <h5 className="card-title"><Link  className="link-success text-decoration-none" to="/dashboard/kids">Kids</Link></h5>
-                        <p className="card-text">Welcome to Kids Section.</p>
+                        <h5 className="card-title text-dark">Kids</h5>
+                        <p className="card-text text-secondary">Welcome to Kids Section.</p>
+                        <a href="#" class="btn btn-danger"><Link  className="link-light text-decoration-none" to="/dashboard/kids">Explore</Link></a>
                     </div>
                     </div>
                 </div>
                         
                 <div className="col">
-                        <div className="card h-100">    
+                        <div className="card h-100 bg-secondary bg-opacity-25">  
+                        <img src="https://storage.alboom.ninja/sites/15438/galleries/47724/studio-emma-dublin-photographer-newborn-dublin-maternity_pregnant-babies-photography-dublin-cake-smash-photoshoot-dublin-photographer-ireland-baby-first-year-photoshoot_58581b932bb8.jpg?t=1582075010" class="img-fluid" alt="Card image cap"></img>  
                         <div className="card-body">
-                        <h5 className="card-title"><Link className="link-success text-decoration-none" to="/dashboard/babies">Babies</Link></h5>
-                        <p className="card-text">Welcome to Babies Section.</p>
+                        <h5 className="card-title text-dark">Babies</h5>
+                        <p className="card-text text-secondary">Welcome to Babies Section.</p>
+                        <a href="#" class="btn btn-danger"><Link className="link-light text-decoration-none" to="/dashboard/babies">Explore</Link></a>
                     </div>
                     </div>
                 </div>
                         
                 <div className="col">
-                        <div className="card h-100">
+                        <div className="card h-100 bg-secondary bg-opacity-25">
+                        <img src="https://img.freepik.com/premium-photo/flat-lay-with-women-accessories_72402-379.jpg?w=2000" class="img-fluid" alt="Card image cap"></img>
                         <div className="card-body">
-                        <h5 className="card-title"><Link className="link-success text-decoration-none" to="/dashboard/accessories">Accessories</Link></h5>
-                        <p className="card-text">Welcome to Accessories Section.</p>
+                        <h5 className="card-title text-dark">Accessories</h5>
+                        <p className="card-text text-secondary">Welcome to Accessories Section.</p>
+                        <a href="#" class="btn btn-danger"><Link className="link-light text-decoration-none" to="/dashboard/accessories">Explore</Link></a>
                     </div>
                     </div>
                 </div>
@@ -73,8 +87,9 @@ class Dashboard extends Component {
                 </div>
                 <br/>
                 <div className='d-grid'>
-                <button type="button" className="btn btn-danger" ><Link className="text-white text-decoration-none" to="/sign-in">Logout</Link></button>
+                
                 </div>
+            
                 </div>
         );
     }
